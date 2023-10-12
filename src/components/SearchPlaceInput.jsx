d8a6224e9fba1a9340d176ecc685760e9ef531e7
cheolung12 <109952479+cheolung12@users.noreply.github.com>
2023-10-12 20:27:19 +0900

    [feat] autocomplete

--- src/components/SearchPlaceInput.jsx
+++ src/components/SearchPlaceInput.jsx
@@ -2,33 +2,56 @@ import React, { useState } from 'react';
 import MapContainer from './MapContainer';
 import { v4 as uuidv4 } from 'uuid';
 
+const { kakao } = window;
+
 export default function SearchPlaceInput() {
   const [inputText, setInputText] = useState('');
   const [places, setPlaces] = useState([]);
   const [isClicked, setIsClicked] = useState(false);
+  const [autoComplete, setAutoComplete] = useState([]);
+
+  const ps = new kakao.maps.services.Places();
 
   const onChange = (e) => {
     setInputText(e.target.value);
+    ps.keywordSearch(e.target.value, placesSearchCB);
+    function placesSearchCB(data, status) {
+      if (status === kakao.maps.services.Status.OK) {
+        setAutoComplete(data.map((el) => el = el.address_name));
+      }
+    }
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     setPlaces([...places, inputText]);
     setInputText('');
+    setAutoComplete([]);
   };
+
   const showMap = () => setIsClicked((prev) => !prev);
+
+  const handleAddress = (address) => {
+    setInputText(address);
+    setAutoComplete([]);
+  }
 
   console.log(places);
   return (
     <div>
-     {!isClicked &&  <form onSubmit={handleSubmit}>
-        <input
-          placeholder='주소를 입력해주세요.'
-          onChange={onChange}
-          value={inputText}
-        />
-        <button type='submit'>검색</button>
-      </form>}
+      {!isClicked && (
+        <form onSubmit={handleSubmit}>
+          <input
+            placeholder='주소를 입력해주세요.'
+            onChange={onChange}
+            value={inputText}
+          />
+          <button type='submit'>검색</button>
+          {autoComplete.map((el) =>
+            <div key={uuidv4()} onClick={() => handleAddress(el)}>{el}</div>
+          )}
+        </form>
+      )}
       {places.map((place, idx) => {
         return (
           <div key={uuidv4()}>
