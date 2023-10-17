45a155106f7a44de638b29d1e6bf47989bae237b
cheolung12 <109952479+cheolung12@users.noreply.github.com>
2023-10-18 04:16:53 +0900

    [edit] router

--- src/index.js
+++ src/index.js
@@ -3,11 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 import NewSchedule from './pages/NewSchedule';
-import MyCalendar from './pages/MyCalendar';
-import Recommendation from './pages/Recommendation';
 import FindLocation from './pages/FindLocation';
 import { Provider } from 'react-redux';
 import store from './store';
@@ -18,10 +15,7 @@ const router = createBrowserRouter([
     element: <App />,
     errorElement: <NotFound />,
     children: [
-      { index: true, element: <Home /> },
-      { path: 'schedule', element: <NewSchedule /> },
-      { path: 'calendar/:userId', element: <MyCalendar /> },
-      { path: 'recommend', element: <Recommendation /> },
+      { index: true, element: <NewSchedule /> },
       { path: 'findlocation', element: <FindLocation /> },
     ],
   },
