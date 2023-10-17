5077c52dcd822f6c45108aedbf97bf1404561ae9
cheolung12 <109952479+cheolung12@users.noreply.github.com>
2023-10-18 04:18:42 +0900

    [add] logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, Link } from 'react-router-dom';
+import Wrapper from './components/Wrapper';
+import { AiOutlineCalendar } from 'react-icons/ai';
 
 function App() {
   return (
-    <>
-      <h1>안녕</h1>
-      <Outlet />
-    </>
+    <div>
+      <div className='absolute top-10 left-1/2 transform -translate-x-1/2'>
+        <Link to='/'>
+          <span className='flex items-center font-LogoFont text-4xl text-orange-500'>
+            내일어때
+            <AiOutlineCalendar />
+          </span>
+        </Link>
+      </div>
+      <Wrapper>
+        <Outlet />
+      </Wrapper>
+    </div>
   );
 }
 
