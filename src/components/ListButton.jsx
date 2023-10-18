17ca1a5a8ab844c53cc48be4703a3324f619a26f
cheolung12 <109952479+cheolung12@users.noreply.github.com>
2023-10-19 04:12:51 +0900

    [fix] class->className

--- src/components/ListButton.jsx
+++ src/components/ListButton.jsx
@@ -6,7 +6,7 @@ export default function ListButton({ status, handleAddInput, handleDelete }) {
       {status === 'add' ? (
         <button
           onClick={handleAddInput}
-          class='inline-flex items-center px-2 py-0.5 text-gray-500 bg-gray-100 rounded-full hover:bg-gray-200 hover:text-gray-600'
+          className='inline-flex items-center px-2 py-0.5 text-gray-500 bg-gray-100 rounded-full hover:bg-gray-200 hover:text-gray-600'
         >
           +
         </button>
