2fbf67656f054403520e0a3a1bf8e4200a4ad39e
cheolung12 <109952479+cheolung12@users.noreply.github.com>
2023-10-19 03:26:07 +0900

    [feat] delete time

--- src/components/ListButton.jsx
+++ src/components/ListButton.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 
-export default function ListButton({status, handleAddInput}) {
-    return (
-        <div>
-            {status === 'add'
-            ? <button onClick={handleAddInput}>+</button>
-            : <button >x</button>
-            }
-        </div>
-    );
+export default function ListButton({ status, handleAddInput, handleDelete }) {
+  return (
+    <div>
+      {status === 'add' ? (
+        <button
+          onClick={handleAddInput}
+          class='inline-flex items-center px-2 py-0.5 text-gray-500 bg-gray-100 rounded-full hover:bg-gray-200 hover:text-gray-600'
+        >
+          +
+        </button>
+      ) : (
+        <button onClick={handleDelete} className='ml-1 px-2 py-1 text-gray-600'>x</button>
+      )}
+    </div>
+  );
 }
-
